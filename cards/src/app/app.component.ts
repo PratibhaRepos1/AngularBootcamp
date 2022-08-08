dece5055fb49c2ab364af6e0620d3747a8e83286
Pratibha <pratibha.jadhav3229@gmail.com>
2022-08-08 16:24:18 +0700

    display cards using Input property

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: './assets/images/tree.jpeg',
+      username: 'nature',
+      content: 'Saw this awesome tree during my hike today.'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: './assets/images/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of snowy mountain.'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: './assets/images/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some biking today.'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: './assets/images/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some biking today.'
+    }
+  ];
 }
