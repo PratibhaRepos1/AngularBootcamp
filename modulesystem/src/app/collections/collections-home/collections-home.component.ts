220533e9eadc1b349c76d500a11ab03aa8f7458f
Pratibha <pratibha.jadhav3229@gmail.com>
2022-08-15 19:08:57 +0700

    reusable table component

--- modulesystem/src/app/collections/collections-home/collections-home.component.ts
+++ modulesystem/src/app/collections/collections-home/collections-home.component.ts
@@ -9,6 +9,20 @@ export class CollectionsHomeComponent implements OnInit {
 
   constructor() { }
 
+  data = [
+    {name: 'John', age: 23, job: 'Designer', employed: true },
+    {name: 'Peter', age: 43, job: 'Manager', employed: true},
+    {name: 'Steve', age: 28, job: 'HR', employed: true},
+    {name: 'Killy', age: 33, job: 'Designer', employed: false}
+  ];
+
+  headers = [
+    {key: 'employed', label: 'Has a Job?'},
+    {key: 'name', label: 'Name'},
+    {key: 'age', label: 'Age'},
+    {key: 'job', label: 'Job'}
+  ];
+
   ngOnInit(): void {
   }
 
