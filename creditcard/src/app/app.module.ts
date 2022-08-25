37425f6ad7fe6a5ede8f2930e0342a5adab9182f
Pratibha <pratibha.jadhav3229@gmail.com>
2022-08-25 19:19:06 +0700

    credit card app initial setup

---
+++ creditcard/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { CardFormComponent } from './card-form/card-form.component';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
